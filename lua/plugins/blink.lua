9c487ee5b1a6d25e55d76a39aa02af41a269052c
Andy Chiem <andy.chiem19@gmail.com>
2026-07-16 14:45:23 -0400

    Remove suggestions during comments

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,7 +2,19 @@ return {
   'saghen/blink.cmp',
   version = '1.*',
   opts = {
-    keymap = { preset = 'super-tab' }, -- Tab to select/confirm, like VSCode
-    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+    keymap = { preset = 'super-tab' },
+    sources = {
+      default = function()
+        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+        local ok, node = pcall(vim.treesitter.get_node, { pos = { row - 1, math.max(0, col - 1) } })
+        if ok and node and vim.tbl_contains(
+          { 'comment', 'line_comment', 'block_comment', 'comment_content' },
+          node:type()
+        ) then
+          return {} -- or 'buffer'
+        end
+        return { 'lsp', 'path', 'snippets', 'buffer' }
+      end,
+    },
   },
 }
