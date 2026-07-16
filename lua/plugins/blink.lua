638742f33b16d422bf38ef198040ecf70a0d0b2d
Andy Chiem <andy.chiem19@gmail.com>
2026-07-16 15:14:06 -0400

    Add general substring check for comment nodes
    Fixed typo
    Add general substring check for comment nodes

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,14 +7,11 @@ return {
       default = function()
         local row, col = unpack(vim.api.nvim_win_get_cursor(0))
         local ok, node = pcall(vim.treesitter.get_node, { pos = { row - 1, math.max(0, col - 1) } })
-        if ok and node and vim.tbl_contains(
-          { 'comment', 'line_comment', 'block_comment', 'comment_content' },
-          node:type()
-        ) then
-          return {} -- or 'buffer'
-        end
+        if ok and node and node:type():match('comment') then
+          return {} -- or buffer
+          end
         return { 'lsp', 'path', 'snippets', 'buffer' }
       end,
-    },
-  },
+},
+},
 }
